773842bbdb3dcc33cd6fb9bc5c0ad54fd4e262a8
CSD-coder <csdyouths@protonmail.com>
2026-05-18 07:35:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/crawl', async (req, res) => {
 
   const id = await crawl(target);
 
-  res.send(`Crawl complete. View at https://searchmium-crawler./crawled.html?id=${id}`);
+  res.send(`Crawl complete. View at https://searchmium-crawler1.onrender.com/crawled.html?id=${id}`);
 });
 
 app.get('/crawled.html', (req, res) => {
